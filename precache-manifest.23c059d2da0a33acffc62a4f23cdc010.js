f449453409ccc2c504c12b7fd5b36807370f38aa
sahil-github29 <sahil242927@gmail.com>
2020-05-29 03:52:08 +0530

    Updates

---
+++ precache-manifest.23c059d2da0a33acffc62a4f23cdc010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9898f60f6780a1792a1ca491567502ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17f61f7b004851b54ba1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2de953521759d1019ccf",
+    "url": "/static/js/2.740f6487.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.740f6487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f61f7b004851b54ba1",
+    "url": "/static/js/main.c0caf9fe.chunk.js"
+  },
+  {
+    "revision": "202af2d4ab67421c18d4",
+    "url": "/static/js/runtime-main.373bdb68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
